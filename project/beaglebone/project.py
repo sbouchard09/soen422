e522b24896bf011a9a9ec34104597bfc4166baaf
Scott Bouchard <scottbouchard09@gmail.com>
2018-11-25 10:40:32 -0500

    added python methods for beaglebone #3

---
+++ project/beaglebone/project.py
@@ -0,0 +1,16 @@
+#import struct, fcntl, os
+#import Adafruit_BBIO.GPIO as GPIO
+
+def get_distance(fd):
+    # returns value from sensor
+    return
+
+def send_direction(fd, t):
+    # sends direction motor should travel in
+    return
+
+def main():
+    #
+    return
+
+main()
